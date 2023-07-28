32fea0305575b902859a78ddc20f1fabc98d3ab9
alienIDK <140614916+alienIDK@users.noreply.github.com>
2023-07-29 05:03:33 +1000

    Update M1.T1P.cpp

--- Module1/M1.T1P.cpp
+++ Module1/M1.T1P.cpp
@@ -1,1 +1,32 @@
 
+// C++ code
+//
+int LED_pin = 9; // initialize LED pin 
+int motion_pin = 10; // initialize motion sensor pin 
+ 
+void setup()
+{
+  pinMode(LED_pin, OUTPUT); // Set LED as output
+  pinMode (motion_pin, INPUT); // Set motion sensor as input
+  digitalWrite(LED_pin, LOW); // Ensure LED is off 
+  Serial.begin(9600); // initialize serial
+  
+}
+
+void loop()
+{
+  int state = digitalRead(motion_pin); // Read motion sensor state
+  if (state == HIGH) // If motion detected 
+  {
+    digitalWrite(LED_pin, HIGH); // turn on LED
+    Serial.println("Motion: Detected, LED: On");
+  }
+  else // if motion NOT detected 
+  {
+    digitalWrite(LED_pin, LOW); // turn off LED
+    Serial.println("Motion: NOT-Detected, LED: Off");
+  }
+  delay(1000); // Pause for a second
+    
+    
+}
